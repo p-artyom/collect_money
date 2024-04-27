5337aaa83b7b20200df84753046c9d70df9a684f
Pilipenko Artem <121739130+p-artyom@users.noreply.github.com>
2024-04-27 11:06:27 +0800

    Update test_data.py

--- backend/money/management/commands/test_data.py
+++ backend/money/management/commands/test_data.py
@@ -1,15 +1,34 @@
+import random
+
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
 from core.factory import CollectFactory, PaymentFactory, UserFactory
+from core.validators import REASONS
+from money.models import Collect
 
 
 class Command(BaseCommand):
     help = 'Создать тестовые данные'
 
     def handle(self, *args, **options):
+        User = get_user_model()
+
         for _ in range(5):
             UserFactory.create()
         for _ in range(50):
-            CollectFactory.create()
+            CollectFactory.create(
+                author=User.objects.all()[
+                    random.randint(0, User.objects.count() - 1)
+                ],
+                reason=random.choice(REASONS)[0],
+            )
         for _ in range(2000):
-            PaymentFactory.create()
+            PaymentFactory.create(
+                author=User.objects.all()[
+                    random.randint(0, User.objects.count() - 1)
+                ],
+                collect=Collect.objects.all()[
+                    random.randint(0, Collect.objects.count() - 1)
+                ],
+            )
