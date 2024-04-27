a82b6ef2778145c70ccbceb04befa0d19f588db8
Pilipenko Artem <121739130+p-artyom@users.noreply.github.com>
2024-04-27 11:26:00 +0800

    Update test_data.py

--- backend/money/management/commands/test_data.py
+++ backend/money/management/commands/test_data.py
@@ -16,19 +16,15 @@ class Command(BaseCommand):
 
         for _ in range(5):
             UserFactory.create()
+        authors_data = User.objects.all()
         for _ in range(50):
             CollectFactory.create(
-                author=User.objects.all()[
-                    random.randint(0, User.objects.count() - 1)
-                ],
+                author=random.choice(authors_data),
                 reason=random.choice(REASONS)[0],
             )
+        collects_data = Collect.objects.all()
         for _ in range(2000):
             PaymentFactory.create(
-                author=User.objects.all()[
-                    random.randint(0, User.objects.count() - 1)
-                ],
-                collect=Collect.objects.all()[
-                    random.randint(0, Collect.objects.count() - 1)
-                ],
+                author=random.choice(authors_data),
+                collect=random.choice(collects_data),
             )
