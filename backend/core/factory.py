cc1af56ddcb1d4cd00e91b59718d1ba591e1e753
Pilipenko Artem <121739130+p-artyom@users.noreply.github.com>
2024-04-27 10:55:46 +0800

    Update factory.py

--- backend/core/factory.py
+++ backend/core/factory.py
@@ -1,20 +1,17 @@
-import random
-
 import factory
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
-from core.validators import REASONS
 from money.models import Collect, Payment
-
-User = get_user_model()
 
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = User
+        model = get_user_model()
 
     username = factory.Faker('user_name')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
     email = factory.Faker('email')
     password = factory.Faker('password')
 
@@ -23,8 +20,6 @@ class CollectFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Collect
 
-    author = User.objects.all()[random.randint(0, User.objects.count() - 1)]
-    reason = random.choice(REASONS)[0]
     title = factory.Faker('sentence')
     description = factory.Faker('sentence')
     amount_money = factory.Faker('random_int')
@@ -40,9 +35,5 @@ class PaymentFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Payment
 
-    author = User.objects.all()[random.randint(0, User.objects.count() - 1)]
-    text = random.choice(REASONS)[0]
+    text = factory.Faker('sentence')
     amount = factory.Faker('random_int')
-    collect = Collect.objects.all()[
-        random.randint(0, Collect.objects.count() - 1)
-    ]
